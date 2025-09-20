cb5abcd1a4f7607a8ced8ec4235d896adff83d44
Bruno Garibaldi <bruno_garibaldi12@hotmail.com>
2025-09-20 14:29:05 -0300

    app fixed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  
-  <ListadoProductosConBusqueda/>
-
+  <v-app>
+    <v-main>
+      <!-- Acá Vue Router renderiza la vista activa -->
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import LoginView from './views/LoginView.vue'
-
 export default {
   name: 'App',
-  components: {
-    LoginView
-  }
 }
 </script>
